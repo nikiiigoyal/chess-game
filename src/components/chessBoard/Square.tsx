85085e13d97b255025f3124a532feb772236ec93
nikiiigoyal <nikiiigoyal@gmail.com>
2025-09-04 12:44:57 +0530

    Add new component square

---
+++ src/components/chessBoard/Square.tsx
@@ -0,0 +1,24 @@
+import { SquareProps } from "../../../utils/chess";
+import Pieces from "./Pieces";
+
+const Square: React.FC<SquareProps> = ({row,col,isLight,squareName,piece,onSquareClick}) => {
+    return (
+        <>
+        <div className={`
+        w-8 h-8 sm:w-12 sm:h-12 md:w-16 md:h-16
+        flex items-center justify-center
+        border border-gray-300
+        cursor-pointer
+        transition-colors duration-200
+        ${isLight ? 'bg-[hsl(62.14deg,42.42%,87.06%)]' : 'bg-[hsl(90.45deg,29%,45.29%)]'}
+        hover:opacity-80
+      `}
+      title={squareName}
+      onClick={() => onSquareClick(row, col, squareName)}
+    >
+   <Pieces piece={piece}/>
+        </div>
+        </>
+    )
+}
+export default Square;
